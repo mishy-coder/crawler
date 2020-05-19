d1ff9a05a044e13bf84c19df207a3b6174b1a703
mishy-coder <52560310+mishy-coder@users.noreply.github.com>
2020-05-19 10:27:32 +0530

    Update wikipp.py

--- wikipp.py
+++ wikipp.py
@@ -1,7 +1,4 @@
-#import wikipediaapi
-#wiki_wiki = wikipediaapi.Wikipedia('en')
-#page_py = wiki_wiki.page('Python_(programming_language)')
-#print(page_py)
+
 import wikipediaapi
 wiki_wiki = wikipediaapi.Wikipedia('en')
 page_py = wiki_wiki.page('Python_(programming_language)')
